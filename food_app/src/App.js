7f6e203b501fbcb29be21800194d4c3091dd887a
aashiagrawal <aashia2@illinois.edu>
2022-10-15 14:56:27 -0500

    connected home page

--- food_app/src/App.js
+++ food_app/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import './index.css'
 import SignUp from "./components/SignUp";
 import LogIn from "./components/LogIn";
+import Home from "./Home";
 import { Maps } from './components/GoogleMapsApi';
 import { Router, Link } from '@reach/router';
 import { BrowserRouter, Routes, Route} from "react-router-dom";
@@ -13,7 +14,7 @@ function App() {
     <Router> 
         <LogIn path="/log_in" default/>
         <SignUp path="/sign_up"/>
-
+        <Home path="/home"/>
     </Router>
     {/* <BrowserRouter>
       <Routes>
