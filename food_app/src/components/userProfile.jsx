2046aa1f116ade246e95aba439458651b471ea28
elisacarrillo <elisamc2@illinois.edu>
2022-11-18 23:55:58 -0500

    attempt to connect to firebase

--- food_app/src/components/userProfile.jsx
+++ food_app/src/components/userProfile.jsx
@@ -1,5 +1,14 @@
+import { FirebaseError } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { ref, set } from "firebase/database";
+import { getDatabase, ref, set , onValue, on} from "firebase/database";
+import { db } from "../base";
+import { ref as sRef } from 'firebase/storage';
+// import { firebase } from "firebase/database"
+import React, { useEffect, useState } from 'react';
+import { useEditableControls, useRangeSlider } from "@chakra-ui/react";
+import { Reorder } from "framer-motion";
+// import { ref } from "firebase/database";
+// import { useDatabaseSnapshot } from "@react-query-firebase/database";
 // const auth = getAuth();
 // const user = auth.currentUser;
 // if (user !== null) {
@@ -16,14 +25,70 @@ import { ref, set } from "firebase/database";
 // //   ProfileComponent();
 //   console.log(uid);
 // }
+// const getUser = user => {
+//     return db.ref('users/${user.uid}')
+// }
+
+const getName = (newtRef) => {
+    return newtRef.data();
+}
 
 const UsrProfile = (props) => {
  
         const auth = getAuth();
         const user = auth.currentUser;
-        var pageTitle = 'Welcome, { user.email } ';
-        console.log(pageTitle);
         console.log(user.email);
+        // console.log("ellie");
+        // con  st newRef = db.getReference('users');
+        
+        // console.log(ref);
+        
+        // const ref = ref(db, "users/" + user.uid)
+        // newRef.child("firstname").once('value', getuserName);
+        // function getuserName(snapshot) {
+        //     // snapshot.forEach(userSnapshot => {
+
+        //     // })
+        //     var val = snapshot.val();
+        //     console.log(val);
+        // }
+        const rref = ref(db, "/users/" + user.uid +"/firstName/");
+        // const db = getDatabase();
+        // var ref = new Firebase("https://food-diary-cs222-default-rtdb.firebaseio.com/users/Xyrgv4DxTzZNbIBq9CirbEqWnFQ2/firstName");
+        // const ref = db.ref("users/" + user.uid  + "/firstName/");
+        // console.log();
+        // const newtRef = db.ref();
+        // ref.on('value', (snapshot) => {
+        //     console.log("hello");
+          
+        //   }); 
+        console.log(rref.val);
+        rref.on('value', (snapshot) => {
+            console.log(snapshot.val());
+          }, (errorObject) => {
+            console.log('The read failed: ' + errorObject.name);
+          });
+        var pageTitle = 'Welcome, { user.email } ';
+        // const query = useDatabaseSnapshot(["fname", "s"], ref)
+        // console.log(ref.firstName);
+        // console.log(ref);
+        // console.log(document.getElementById('firstName'));
+        const userId = user.uid;
+        // console.log(ref);
+        // newtRef.once("value").then(nameSnapshot => nameSnapshot.val())
+        // getName(newtRef)
+        //     .then(firstName => { 
+        //         /* ... do something ... */ 
+        //         console.log("here");
+        //     })
+        //     .catch(err => { 
+        //         /* ... handle errors ... */ 
+        //         console.log(err)
+        // });
+        
+        // newRef.addListenerForSingleValueEvent(new ValueEventListener() {
+        // i cant hear u the song is too good not taking off headphones
+        // })
         
         return (
             // <h2>"Hello"</h2>
