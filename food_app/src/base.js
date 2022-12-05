117c07ef3e56e0019ad854071016fb9f09c161dc
“madebayo1023” <Adebayo5@illinois.edu>
2022-12-05 03:00:37 -0600

    images connected to database

--- food_app/src/base.js
+++ food_app/src/base.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
 import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
@@ -21,6 +22,7 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
 export const db = getDatabase(app);
 export const firestore = getFirestore(app);
+export const storage = getStorage(app);
 // const firesto
 
 export default app;
